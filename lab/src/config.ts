0847dd2d8bcb624a8406a9d88c60a33828cc5ac4
Bjørn-Erik Strand <bjoern.erik.strand@gmail.com>
2026-06-04 12:50:12 +0200

    More generic readme for server config (#58)

--- lab/src/config.ts
+++ lab/src/config.ts
@@ -219,7 +219,7 @@ export function createPlaygroundConfig(): Configuration {
       kind: 'inference',
       name: 'default',
       spec: {
-        name: 'envoy-default',
+        name: 'remote',
         endpoint: 'http://localhost:8091',
       },
     },
